5f0e0504bc1a4fad7c21b648aa5af0455eb39f74
Esowol <innigold@gmail.com>
2022-05-23 12:20:06 +0100

    progress3

--- js/scripts.js
+++ js/scripts.js
@@ -26,7 +26,7 @@
     });
 
     // offcanvas script from Bootstrap + added element to close menu on click in small viewport
-    $('[data-toggle="offcanvas"], .navbar-nav li a:not(.dropdown-toggle').on('click', function () {
+    $('[data-toggle=nav"offcanvas"], .navbar-nav li a:not(.dropdown-toggle').on('click', function () {
         $('.offcanvas-collapse').toggleClass('open')
     })
 
